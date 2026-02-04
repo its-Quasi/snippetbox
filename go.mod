c04b74c29cb54da3bf3d7a29868aadbf61e19d76
Felipe Alferez <alferezfelipe@gmail.com>
2026-02-04 13:21:31 -0500

    feat: add sign up and email validation

--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module snippetbox.quasi.go
 go 1.25.4
 
 require (
+	github.com/alexedwards/scs/postgresstore v0.0.0-20251002162104-209de6e426de
+	github.com/alexedwards/scs/v2 v2.9.0
 	github.com/go-playground/form/v4 v4.3.0
 	github.com/justinas/alice v1.2.0
 	github.com/lib/pq v1.10.9
-)
-
-require (
-	github.com/alexedwards/scs/postgresstore v0.0.0-20251002162104-209de6e426de // indirect
-	github.com/alexedwards/scs/v2 v2.9.0 // indirect
+	golang.org/x/crypto v0.47.0
 )
--- internal/models/users.go
+++ internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -18,6 +23,24 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	stmt := `
+		INSERT INTO users (name, email, hashed_password)
+		VALUES($1, $2, $3)
+	`
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		var pgErr *pq.Error
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == "23505" && strings.Contains(pgErr.Constraint, "users_email_key") {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 
